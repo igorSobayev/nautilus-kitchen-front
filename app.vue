33b15d44fc7b33e9f948c734383bcb6f5b08f2fa
Igor <isobayev@gmail.com>
2023-08-25 18:09:31 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-[#001e26] dark flex-col">
+    <x-button color="primary">Filled</x-button>
+    <x-button color="secondary">Filled</x-button>
+    <x-button color="success">Filled</x-button>
+    <x-button color="warning">Filled</x-button>
+    <x-button color="error">Filled</x-button>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+import { defineNuxtConfig } from 'nuxt'
+import { colors } from '@indielayer/ui'
+import icons from './icons'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    ['@indielayer/ui/nuxt', {
+      icons,
+      prefix: 'X',
+      theme: {
+        classPrefix: 'x-',
+        colors: {
+          primary: colors.indigo,
+          secondary: colors.sky,
+          success: colors.lime,
+          warning: colors.yellow,
+          error: colors.red,
+        },
+      }
+    }],
+  ],
+  css: ['~/assets/tailwind.css'],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          'tailwindcss/nesting': {},
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+})
