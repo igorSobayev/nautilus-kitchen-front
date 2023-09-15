02a99f35f890c76c125274e6528050879c4c5daf
Igor <isobayev@gmail.com>
2023-09-15 12:22:07 +0200

    Ingredients

---
+++ utils/shared.js
@@ -0,0 +1,130 @@
+export const ingredients = [
+    {
+        cat_key: 'meats_and_proteins',
+        icon: '',
+        ingredients: [
+            { key: 'chicken', icon: '' },
+            { key: 'beef', icon: '' },
+            { key: 'pork', icon: '' },
+            { key: 'turkey', icon: '' },
+            { key: 'fish', icon: '' },
+            { key: 'seafood', icon: '' },
+            { key: 'tofu', icon: '' },
+            { key: 'eggs', icon: '' },
+            { key: 'ground_beef', icon: '' },
+            { key: 'turkey_breast', icon: '' }
+        ]
+    },
+    {
+        cat_key: 'vegetables',
+        icon: '',
+        ingredients: [
+            { key: 'tomatoes', icon: '' },
+            { key: 'carrots', icon: '' },
+            { key: 'broccoli', icon: '' },
+            { key: 'spinach', icon: '' },
+            { key: 'onions', icon: '' },
+            { key: 'peppers', icon: '' },
+            { key: 'mushrooms', icon: '' },
+            { key: 'zucchinis', icon: '' },
+            { key: 'cucumbers', icon: '' },
+            { key: 'lettuce', icon: '' }
+        ]
+    },
+    {
+        cat_key: 'fruits',
+        icon: '',
+        ingredients: [
+            { key: 'apples', icon: '' },
+            { key: 'bananas', icon: '' },
+            { key: 'strawberries', icon: '' },
+            { key: 'grapes', icon: '' },
+            { key: 'oranges', icon: '' },
+            { key: 'lemons', icon: '' },
+            { key: 'mangos', icon: '' },
+            { key: 'kiwis', icon: '' },
+            { key: 'pears', icon: '' },
+            { key: 'raspberries', icon: '' }
+        ]
+    },
+    {
+        cat_key: 'grains_and_legumes',
+        icon: '',
+        ingredients: [
+            { key: 'rice', icon: '' },
+            { key: 'pasta', icon: '' },
+            { key: 'lentils', icon: '' },
+            { key: 'chickpeas', icon: '' },
+            { key: 'beans', icon: '' },
+            { key: 'quinoa', icon: '' },
+            { key: 'oats', icon: '' },
+            { key: 'bread', icon: '' },
+            { key: 'corn', icon: '' },
+            { key: 'couscous', icon: '' }
+        ]
+    },
+    {
+        cat_key: 'dairy_and_alternatives',
+        icon: '',
+        ingredients: [
+            { key: 'milk', icon: '' },
+            { key: 'cheese', icon: '' },
+            { key: 'yogurt', icon: '' },
+            { key: 'butter', icon: '' },
+            { key: 'almond_milk', icon: '' },
+            { key: 'soy_milk', icon: '' },
+            { key: 'coconut_milk', icon: '' },
+            { key: 'cottage_cheese', icon: '' },
+            { key: 'ricotta_cheese', icon: '' },
+            { key: 'sour_cream', icon: '' }
+        ]
+    },
+    {
+        cat_key: 'oils_and_seasonings',
+        icon: '',
+        ingredients: [
+            { key: 'olive_oil', icon: '' },
+            { key: 'vegetable_oil', icon: '' },
+            { key: 'melted_butter', icon: '' },
+            { key: 'salt', icon: '' },
+            { key: 'black_pepper', icon: '' },
+            { key: 'garlic', icon: '' },
+            { key: 'onion_powder', icon: '' },
+            { key: 'basil', icon: '' },
+            { key: 'rosemary', icon: '' },
+            { key: 'thyme', icon: '' }
+        ]
+    },
+    {
+        cat_key: 'nuts_and_seeds',
+        icon: '',
+        ingredients: [
+            { key: 'almonds', icon: '' },
+            { key: 'walnuts', icon: '' },
+            { key: 'chia_seeds', icon: '' },
+            { key: 'sunflower_seeds', icon: '' },
+            { key: 'pumpkin_seeds', icon: '' },
+            { key: 'peanuts', icon: '' },
+            { key: 'hazelnuts', icon: '' },
+            { key: 'cashews', icon: '' },
+            { key: 'pistachios', icon: '' },
+            { key: 'shredded_coconut', icon: '' }
+        ]
+    },
+    {
+        cat_key: 'spices_and_herbs',
+        icon: '',
+        ingredients: [
+            { key: 'turmeric', icon: '' },
+            { key: 'cumin', icon: '' },
+            { key: 'paprika', icon: '' },
+            { key: 'cinnamon', icon: '' },
+            { key: 'oregano', icon: '' },
+            { key: 'ginger', icon: '' },
+            { key: 'clove', icon: '' },
+            { key: 'coriander', icon: '' },
+            { key: 'parsley', icon: '' },
+            { key: 'tarragon', icon: '' }
+        ]
+    }
+]
