58e69e1548466b2e94caeddf9a59a42316d18684
Igor <isobayev@gmail.com>
2023-11-12 16:53:08 +0100

    Fix rating component position

--- components/custom/NKUserProfileInfo.vue
+++ components/custom/NKUserProfileInfo.vue
@@ -23,9 +23,9 @@ onNuxtReady(async () => {
           <div :style="'background-image: url(' + user.avatar + ');'" class="bg-cover bg-center bg-no-repeat rounded-full h-64 w-64"></div>
       </div>
       <div class="flex flex-col gap-4 xl:border-r border-[#e5e7eb]">
-          <h2 class="text-4xl mb-4">{{ user.name }}</h2>
+          <h2 class="text-4xl mb-4">{{ user.username }}</h2>
           <div class="flex items-center gap-3"><span class="">Recetas</span> <UBadge color="primary" variant="solid" :label="user.recipesNumber ?? 0" /></div>
-          <div class="flex items-center gap-3">Valoracón media <NuxtRating :read-only="true" :ratingValue="user.avgRating ?? 0" /></div>
+          <div class="flex items-center gap-3"><NuxtRating :read-only="true" :ratingValue="user.avgRating ?? 0" /></div>
           <div>Activo desde <span class="font-bold">{{ userCreationDate }}</span></div>
       </div>
       <div class="flex border-t border-[#e5e7eb] xl:border-t-0 xl:m5-0 xl:pt-0 xl:pl-5 mt-6 pt-6">{{ user.description }}</div>
