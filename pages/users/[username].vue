b4844870c92e3eb775ac4cc16993a2de72e3d154
Igor <isobayev@gmail.com>
2023-11-25 10:25:01 +0100

    Remove unused recipes load

--- pages/users/[username].vue
+++ pages/users/[username].vue
@@ -3,12 +3,22 @@ import NKUserProfileInfo from '~/components/custom/NKUserProfileInfo.vue'
 import NKUserRecipes from '~/components/custom/publicComps/NKUserRecipes.vue'
 import { useUserStore } from '~/store/user'
 
+const route = useRoute()
+
+useHead({
+  title: route.params.username + '  page',
+  meta: [
+    {
+      name: 'description',
+      content: 'TODO: SEO' 
+    },
+  ],
+})
+
 const userStore = useUserStore()
 
-const publishedRecipes = ref([])
-
 onNuxtReady(async () => {
-    publishedRecipes.value = await userStore.loadUserPublishedRecipes()
+    
 })
 </script>
 
