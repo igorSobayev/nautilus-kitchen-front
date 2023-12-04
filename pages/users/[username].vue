3eaa710046ccd99b4b7073efc2d9aaffd5e551e0
Igor <isobayev@gmail.com>
2023-12-04 18:48:44 +0100

    Remove follow button from upper level

--- pages/users/[username].vue
+++ pages/users/[username].vue
@@ -1,11 +1,8 @@
 <script setup>
 import NKUserProfileInfo from '~/components/custom/NKUserProfileInfo.vue'
 import NKUserRecipes from '~/components/custom/publicComps/NKUserRecipes.vue'
-import { useAuthStore } from '~/store/auth';
-import { useUserStore } from '~/store/user'
 
 const route = useRoute()
-const toast = useToast()
 
 useHead({
   title: route.params.username + '  page',
@@ -17,16 +14,6 @@ useHead({
   ],
 })
 
-const userStore = useUserStore()
-
-const followUser = async () => {
-  const username = route.params.username
-  await userStore.followUser(username)
-    .then(() => {
-      toast.add({ title: `¡Has seguido a ${username}!` })
-    })
-}
-
 onNuxtReady(async () => {
     
 })
@@ -34,11 +21,8 @@ onNuxtReady(async () => {
 
 <template>
     <div class="flex justify-center flex-col gap-2 pt-5">
-        <div class="flex justify-end">
-          <UButton label="Follow user" icon="i-heroicons-user-plus" @click="followUser" size="sm" color="primary" square />
-        </div>
         <div class="flex justify-center flex-col pt-5">
-            <NKUserProfileInfo />
+            <NKUserProfileInfo :public="true" />
         </div>
         <div class="w-full mt-5">
             <NKUserRecipes />
