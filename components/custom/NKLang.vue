997f3e34e95c999057fb7e7576d9caca2def6015
Igor <isobayev@gmail.com>
2023-12-10 13:08:20 +0100

    Lang comp

---
+++ components/custom/NKLang.vue
@@ -0,0 +1,20 @@
+<template>
+    <USelect v-model="locale" :options="langs" />
+</template>
+  
+
+<script setup>
+const langs = ['es', 'en']
+const { locale } = useI18n()
+
+watch(locale, async (newLocale) => {
+    localStorage.setItem('userLangSelection', newLocale)
+})
+
+onNuxtReady(async () => {
+    const userLangSelection = localStorage.getItem('userLangSelection')
+    if (userLangSelection) {
+        locale.value = userLangSelection
+    }
+})
+</script>
